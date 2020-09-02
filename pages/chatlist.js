04b0e028b30b7e2a7d09f32c5dd693aa03a69035
Pop0097 <dhruvrawat09@gmail.com>
2020-09-02 15:37:30 -0400

    Placeholder

--- pages/chatlist.js
+++ pages/chatlist.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import db, { provider, auth } from '../firebase';
+import { useStateValue } from '../stateprovider';
 
 
 function ChatList({ navigation }) {
+
+    const [{ userDoc }, dispatch] = useStateValue(); 
+
     return(
         <TouchableOpacity onPress={() => navigation.navigate("Chat")} >
             <View>
                 <Text> This is the Chat List! </Text>
+                <Text>{ userDoc.name } </Text> 
             </View>
         </TouchableOpacity>
     );
